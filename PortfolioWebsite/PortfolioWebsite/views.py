d0079e5396612b3f43ca0158c9fed4a018ba4f3f
Afnan Rehman <Afnan Rehman@AFNAN-DESKTOP>
2021-10-25 01:00:15 -0400

    Add project files.

---
+++ PortfolioWebsite/PortfolioWebsite/views.py
@@ -0,0 +1,37 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from PortfolioWebsite import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='How to reach me'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='A few things about me'
+    )
